a87e756c5135de7f028c6acbe031742521b803b5
hujguo <160381094+hujguo@users.noreply.github.com>
2022-11-05 06:14:41 +0000

    fix #68

--- Assignment 2/A#2_Homography_and_Image_Stitching.py
+++ Assignment 2/A#2_Homography_and_Image_Stitching.py
@@ -14,4 +14,16 @@ from operator import sub
 PREPATH = 'images/'
 IMNAMES = {
     'cmpe_building': ['left_2', 'left_1', 'middle', 'right_1', 'right_2'],
-    'north_campus': ['left_2', 'left_1', '
+    'north_campus': ['left_2', 'left_1', 'middle', 'right_1', 'right_2'],
+    'paris': ['paris_a', 'paris_b', 'paris_c',],
+    'book': ['book_table', 'book_orig'] # test dataset
+}
+EXT = '.jpg'
+
+NPYDIR = 'data/'
+
+RESDIR = 'results/'
+
+METHODS = ['left-to-right', 'middle-out', 'first-out-then-middle']
+
+#################################

89cabf365fd9407a7e2e4df8f81cd3450c9c7a7a
hujguo <160381094+hujguo@users.noreply.github.com>
2023-04-02 08:23:51 +0000

    Regenerated the reports

--- Assignment 3/classification.py
+++ Assignment 3/classification.py
@@ -8,4 +8,13 @@ from quantization import get_hist
 # y: label for each image
 
 class KNN:
-   
+    def __init__(self, k):
+        self.k = k
+    def fit(self, X, y):
+        self.num_cluster = X.shape[1]
+        self.X = np.copy(X)
+        self.y = np.copy(y)
+        return self
+    def predict(self, X_test):
+        y_predict = np.zeros((X_test.shape[0],))
+        for i, x 

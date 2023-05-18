15e6cffd22a0cdac064fb1383518324d441966e2
hujguo <160381094+hujguo@users.noreply.github.com>
2023-05-18 21:11:20 +0000

    b43: N-PHY: increase initial value of "mind" in RSSI calibration

--- Assignment 3/descriptors.py
+++ Assignment 3/descriptors.py
@@ -2,4 +2,13 @@ import cv2 as cv
 import os
 
 class HOG:
-    def detectAndCompute(
+    def detectAndCompute(self, image, dummy=1):
+        ## This function takes an RGB image as input. First, the image is turned into Gray scale. Then, HOG is calculated.
+        resize=(128,128)
+        grid_size=(8,8)
+        num_of_bins=60
+
+        grid_dim = int(resize[0]/grid_size[0])
+
+        # Resize the image to a fixed size:
+        image = 

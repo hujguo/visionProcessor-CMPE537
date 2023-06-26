28ff6a94e03dc5c6674aef05fd5d207f40cfab7c
hujguo <160381094+hujguo@users.noreply.github.com>
2023-06-26 19:46:01 +0000

    Update 01.html

--- Assignment 3/dictionary.py
+++ Assignment 3/dictionary.py
@@ -5,4 +5,12 @@ import numpy as np
 def get_dictionary(dictname, num_cluster):
     if dictname == 'BOW':
         # https://docs.opencv.org/3.4.2/d4/d72/classcv_1_1BOWKMeansTrainer.html
-        return cv.BOWKMeans
+        return cv.BOWKMeansTrainer(clusterCount=num_cluster)
+    else:
+        raise Exception('Invalid option')
+
+def func_add_descriptors(impath, indices, dictionary, descriptor, desc_per_img=20):
+    img = cv.imread(impath)
+    assert img is not None
+    _, desc = descriptor.detectAndCompute(img, None)
+ 

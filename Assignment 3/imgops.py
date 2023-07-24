237b868af6a644df369a083a1dd090a3eaf24b02
hujguo <160381094+hujguo@users.noreply.github.com>
2023-07-24 21:37:09 +0000

    Adding about.html

--- Assignment 3/imgops.py
+++ Assignment 3/imgops.py
@@ -10,4 +10,12 @@ def cv_imshow_scale(winname, img):
     cv.destroyWindow(winname)
 
 def plt_imshow(img):
-    plt.imshow(cv.cvtColor(i
+    plt.imshow(cv.cvtColor(img, cv.COLOR_BGR2RGB))
+    plt.show()
+
+def loop_images(func,   # function to be called on each image
+                params, # func(impath, (taxon_id, img_id), *params)
+                taxons_path='Dataset/Caltech20/training/',
+                dry_run=True):
+    taxons = os.listdir(taxons_path)
+    for i_t, taxon in enumerate(taxons

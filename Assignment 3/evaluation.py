76c3c97642897640f0c9534a1ea4a9dd3b547a60
hujguo <160381094+hujguo@users.noreply.github.com>
2023-07-24 17:55:38 +0000

    tracing: Fix irqs-off tag display in syscall tracing

--- Assignment 3/evaluation.py
+++ Assignment 3/evaluation.py
@@ -1,1 +1,9 @@
-from sklearn.metrics impo
+from sklearn.metrics import multilabel_confusion_matrix, precision_recall_fscore_support
+import numpy as np
+
+def Scores(y_test, y_predict):
+    precision, recall, F1, support = precision_recall_fscore_support(y_test, y_predict)
+    MeanF1 = np.mean(F1)
+    conf_matrix = multilabel_confusion_matrix(y_test, y_predict)
+
+    return 
